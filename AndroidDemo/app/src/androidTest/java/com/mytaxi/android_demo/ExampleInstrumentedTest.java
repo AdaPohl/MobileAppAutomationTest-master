5436f37510799cdb5d45ef4301e90efdee1d089c
Ada Möhlmann <adamohlmann@adas-macbook.speedport_w_724v_typ_a_05011603_05_028>
2018-02-28 17:54:37 +0100

    Initial commit

---
+++ AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/ExampleInstrumentedTest.java
@@ -0,0 +1,33 @@
+ package com.mytaxi.android_demo;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.mytaxi.android_demo", appContext.getPackageName());
+    }
+
+    @Test
+    public void greeterSaysHello() {
+        onView(withId(R.id.name_field)).perform(typeText("Steve"));
+        onView(withId(R.id.greet_button)).perform(click());
+        onView(withText("Hello Steve!")).check(matches(isDisplayed()));
+    }
+}
